0c272462d7f8599e326f80a54627cfb02215cf31
Your Name <you@example.com>
2014-08-07 09:44:43 -0400

    forgot the root

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'welcome/grade'
 
+  root 'welcome/grade'
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
